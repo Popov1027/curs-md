b6a3f37b95d3cba505bac6886957d0687e865de5
Daniel20 <tatarud24@gmail.com>
2023-07-27 01:45:00 +0300

    am terminat Navigation Bar , am adaugat data.json si am simplificat codul din 160 randuri in 85

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,128 +1,45 @@
 import React, { useState } from 'react';
-// @ts-ignore
-import Home from '../../assets/img/home.svg';
-// @ts-ignore
-import logoCurs from '../../assets/img/logo.png';
-// @ts-ignore
-import HomeMenu from '../../assets/img/logo-inv.png';
-import styled from 'styled-components';
+import Home from '../../assets/images/home.svg';
+import logoCurs from '../../assets/images/logo.png';
+import HomeMenu from '../../assets/images/logo-inv.png';
+import data from './data.json';
 
-const ContainerLogo = styled.div`
-  margin: 40px 80px 20px;
-  height: 35px;
-  width: 190px;
-  @media (max-width: 767px) {
-    margin: 0;
-    padding: 1px 10px;
-  }
-`;
-const NavHeader = styled.nav`
-  @media (max-width: 767px) {
-    margin-top: -35px;
-  }
-`;
-const ContainerMenu = styled.div`
-  @media (max-width: 767px) {
-  }
-`;
-const HomeWhite = styled.div`
-  color: white;
-  background: rgba(155, 155, 155, 0.2);
-  @media (max-width: 767px) {
-    margin-right: auto;
-    margin-top: 110px;
-    margin-left: 2px;
-  }
-`;
-const ImgLogoCurs = styled.img`
-  @media (max-width: 767px) {
-    display: none;
-  }
-`;
-const ImgSvgHome = styled.img`
-  @media (max-width: 767px) {
-    display: none;
-  }
-`;
-const ImgLogoHomeMenu = styled.img`
-  @media (max-width: 767px) {
-    margin-top: 2px;
-  }
-`;
-const DivHome = styled.div`
-  display: flex;
-  color: #cacaca;
-  background: rgba(155, 155, 155, 0.2);
-`;
-const SpanShadow = styled.span`
-  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.25);
-`;
-const DivImgAndButton = styled.div`
-  @media (max-width: 767px) {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-  }
-`;
-const BurgerMenuContainer = styled.div`
-  display: flex;
-  align-items: center;
-
-  @media (max-width: 767px) {
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 15px;
-  }
-`;
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // eslint-disable-next-line no-unused-vars
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <ContainerMenu>
-      <ContainerLogo>
-        <BurgerMenuContainer>
+    <div>
+      <div className="mt-10 mx-20 mb-5 h-9 w-48 mt-[-56px] md:mt-10">
+        <div className="flex items-center md:flex-col md:justify-center ">
           <div className="logo">
-            <ImgLogoCurs src={logoCurs} alt="Curs.md" className="curs-md" />
+            <img src={logoCurs} alt="Curs.md" className="hidden md:block" />
           </div>
-        </BurgerMenuContainer>
-      </ContainerLogo>
-      <NavHeader className="bg-[#3c4059]">
-        <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
+        </div>
+      </div>
+      <nav className="bg-[#3c4059]">
+        <div className="mx-auto max-w-7xl px-2 md:px-6 lg:pl-16">
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start ">
-              <HomeWhite className="flex flex-shrink-0 items-center">
-                <ImgSvgHome src={Home} alt="Home" className="h-7 w-auto cursor-pointer" />
-              </HomeWhite>
+              <div className="flex flex-shrink-0 items-center hover:bg-[#4c5069]">
+                <img src={Home} alt="Home" className="h-5 w-auto cursor-pointer hidden md:block" />
+              </div>
               <div className="hidden md:ml-6 md:block">
-                <div className="flex space-x-4">
-                  <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer">
-                    Rates Evolution
-                  </span>
-                  <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer">
-                    Convertor
-                  </span>
-                  <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer">
-                    Exchange rates
-                  </span>
-                  <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer">
-                    Banks
-                  </span>
-                  <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer">
-                    Exchange offices
-                  </span>
-                  <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer">
-                    Currencies
-                  </span>
+                <div className="flex ">
+                  {data.links.map((link) => (
+                    <a href={link.url} key={link.id}>
+                      <span className="text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer hover:bg-[#4c5069]">
+                        {link.text}
+                      </span>
+                    </a>
+                  ))}
                 </div>
               </div>
             </div>
-            <DivImgAndButton className="md:hidden">
-              <ImgLogoHomeMenu src={HomeMenu} alt="Home" className="mb-2.5 h-6" />
+            <div className="md:hidden flex w-full justify-between">
+              <img src={HomeMenu} alt="Home" className="mb-2.5 h-6" />
               <button
                 type="button"
                 className="text-white hover:text-gray-200 focus:outline-none focus:text-gray-200"
@@ -139,39 +56,28 @@ const Navbar = () => {
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
                 </svg>
               </button>
-            </DivImgAndButton>
+            </div>
           </div>
         </div>
         {isMenuOpen && (
           <div className="px-2 pt-2 pb-3 space-y-1 md:hidden">
-            <DivHome>
+            <div className="bg-[#9b9b9b33] flex text-gray-400">
               <img src={Home} alt="Home" className="h-5 mt-1.5 ml-px w-auto cursor-pointer" />
-              <SpanShadow className="block px-3 py-2 text-sm font-medium cursor-pointer">
+              <span className="block px-3 py-2 text-sm font-medium cursor-pointer hover:bg-[#4c5069]">
                 Exchange rates
-              </SpanShadow>
-            </DivHome>
-            <span className="text-white block px-3 py-2 text-sm font-medium cursor-pointer">
-              Rates Evolution
-            </span>
-            <span className="text-white block px-3 py-2 text-sm font-medium cursor-pointer">
-              Convertor
-            </span>
-            <span className="text-white block px-3 py-2 text-sm font-medium cursor-pointer">
-              Exchange rates
-            </span>
-            <span className="text-white block px-3 py-2 text-sm font-medium cursor-pointer">
-              Banks
-            </span>
-            <span className="text-white block px-3 py-2 text-sm font-medium cursor-pointer">
-              Exchange offices
-            </span>
-            <span className="text-white block px-3 py-2 text-base font-medium cursor-pointer">
-              Currencies
-            </span>
+              </span>
+            </div>
+            {data.links.map((link) => (
+              <a href={link.url} key={link.id}>
+                <span className="text-white block px-3 py-2 text-sm font-medium cursor-pointer hover:bg-[#4c5069]">
+                  {link.text}
+                </span>
+              </a>
+            ))}
           </div>
         )}
-      </NavHeader>
-    </ContainerMenu>
+      </nav>
+    </div>
   );
 };
 
