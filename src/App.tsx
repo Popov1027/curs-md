40b447d229e6cab1543fbb11caab1d667d4f8984
Sergiu_Luchin <sergiusergiuaaa@gmail.com>
2023-07-27 17:04:41 +0200

    add-video

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
